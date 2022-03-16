efcaaec79515de73a6cd1efdbc35ffed2dc83aa3
Eng Md Ektiar Hossen <97474187+engektiar@users.noreply.github.com>
2022-03-16 11:57:15 +0600

    first commit

---
+++ callback.js
@@ -0,0 +1,22 @@
+function explineng_callBack(name,age,text){
+  console.log("hello",name);
+  console.log("Your age =",age)
+  text();
+  gatings();
+}
+function washand(){
+  console.log("Please wash Your Hand");
+}
+
+function facebookuse(){
+  console.log("scroll your facebook ");
+}
+function taka(){
+  console.log("Out of the dore");
+}
+function gatings(){
+  console.log("Thank you son much Dear all freide Please dongt mind my opening taka care yourself dont mind");
+}
+explineng_callBack('jamal uddin',18,washand);
+explineng_callBack('Kamal Hossain',29,facebookuse);
+explineng_callBack('Abdul Malk',30,taka);
